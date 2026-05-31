eca35f60c03e52eb52ea3504473d9b128da55045
naftali-spitz <0012ab@gmail.com>
2026-05-31 21:44:41 +0300

    Extract body scroll lock hook

---
+++ hooks/use-body-scroll-lock.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+export function useBodyScrollLock(locked: boolean) {
+  useEffect(() => {
+    if (locked) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [locked]);
+}
